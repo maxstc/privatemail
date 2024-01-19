234d1fe454d4705c605cd56e2fda9482c2e355c3
maxstc <a@a.a>
2024-01-19 09:13:33 +0000

    linked signup to backend api

--- client/account.js
+++ client/account.js
@@ -2,10 +2,27 @@ function signIn() {
     alert("Signin");
 }
 
+function signUp() {
+    if (document.getElementById("signup-password").value === document.getElementById("signup-confirmpassword").value) {
+        createAccount();
+    }
+    else {
+        alert("Password and confirm password do not match");
+    }
+}
+
 function createAccount() {
-    alert("xd");
+    fetch("auth/signup", {
+        method: "POST",
+        body: JSON.stringify({
+            address: document.getElementById("signup-address"),
+            password: document.getElementById("signup-password")
+        })
+    })
+    .then((response) => response.json())
+    .then((json) => alert(json));
 }
 
 function signOut() {
-    alert("asdf");
+    document.cookie = "auth=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
--- webserver/index.js
+++ webserver/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
-const db = require("../fakedb.json");
+const db = require("./fakedb.json");
 const fs = require("fs");
 
 app.use(express.json());
+app.use(express.static("../client"));
 
 const authRouter = require("./routers/auth.js");
 app.use("/auth", authRouter);
 
 app.get("/", (req, res) => {
-    res.send("xd");
+    res.redirect("index.html");
 });
 
 app.listen(3000, () => console.log("Web server runnning on port 3000"));
