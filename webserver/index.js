41921299b938a6357a3a70300b911f11ce213466
maxstc <a@a.a>
2024-01-21 19:02:56 +0000

    moved mail server to web server file

--- webserver/index.js
+++ webserver/index.js
@@ -1,3 +1,15 @@
+const SMTPServer = require("smtp-server").SMTPServer;
+const server = new SMTPServer({
+    authOptional: true,
+    onData(stream, session, callback) {
+        console.log(stream);
+        stream.pipe(process.stdout);
+        stream.on("end", callback);
+    }
+});
+server.listen(3001);
+console.log("SMTP Mail server running on port 3002");
+
 const express = require("express");
 const app = express();
 
---
+++ webserver/routers/mail.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../fakedb.json");
+const fs = require("fs");
